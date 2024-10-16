2ed5fe1b917f74992a1d588b4cbce2c53984394c
olegblackwall <olegblackwall@mail.ru>
2024-10-16 23:30:03 +0700

    initial project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,6 @@
 export default defineNuxtConfig({
-    ssr: true,
     compatibilityDate: '2024-04-03',
     devtools: { enabled: false },
     css: ['bootstrap/dist/css/bootstrap.min.css'],
     modules: ['@pinia/nuxt'],
-    app: {
-        baseURL: '/pet-automobily-nuxt3-pinia/',
-        head: {
-            link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-            ]
-        },
-    }
 })
